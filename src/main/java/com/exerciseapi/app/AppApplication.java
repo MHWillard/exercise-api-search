23235b9357a1af7076654d191509afb5a074c2d0
MHWillard <mhwillard@gmail.com>
2022-12-13 15:55:37 -0500

    .

--- src/main/java/com/exerciseapi/app/AppApplication.java
+++ src/main/java/com/exerciseapi/app/AppApplication.java
@@ -1,22 +1,39 @@
 package com.exerciseapi.app;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
+//@RestController
 public class AppApplication {
+
+	private static final Logger log = LoggerFactory.getLogger(AppApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppApplication.class, args);
 	}
 
-	@GetMapping("/hello")
-	public String hello(@RequestParam(value = "name", defaultValue = "world") String name) {
-		return String.format("Hello %s!", name);
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
+
+	@Bean
+	public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+		return args -> {
+			Quote quote = restTemplate.getForObject(
+					"http://localhost:8080/api/random", Quote.class);
+			log.info(quote.toString());
+		};
 	}
 
 }
