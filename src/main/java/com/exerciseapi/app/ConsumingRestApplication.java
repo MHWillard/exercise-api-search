dd7d2621d628e4b00c49d8367d1333fde995aacb
MHWillard <mhwillard@gmail.com>
2022-12-21 16:08:48 -0500

    12.21.22 new route

--- src/main/java/com/exerciseapi/app/ConsumingRestApplication.java
+++ src/main/java/com/exerciseapi/app/ConsumingRestApplication.java
@@ -31,12 +31,8 @@ public class ConsumingRestApplication {
 			// https://openlibrary.org/developers/api
 			// https://openlibrary.org/search.json?title=vote+for+duck+click+clack+moo
 
-			Entry entry = restTemplate.getForObject("https://openlibrary.org/search.json?title=vote+for+duck+click+clack+moo", Entry.class);
-			//Docs docs = restTemplate.getForObject("https://openlibrary.org/search.json?title=vote+for+duck+click+clack+moo", Docs.class);
-			//Entry entry = restTemplate.getForObject("https://openlibrary.org/books/OL7353617M.json", Entry.class);
+			Entry entry = restTemplate.getForObject("https://openlibrary.org/search.json?q=vote+for+duck+click+clack+moo", Entry.class);
 			log.info(entry.toString());
-			System.out.println("breakpoint");
-			//log.info(docs.toString());
 		};
 	}
 
@@ -47,5 +43,26 @@ public class ConsumingRestApplication {
 	@GetMapping("/hello")
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return String.format("Hello %s!", name);
+	}
+
+	@GetMapping("/json")
+	public String json(@RequestParam(value = "searchTerm", defaultValue = "lord+of+the+rings") String searchTerm) {
+		//return String.format("Hello %s!", name);
+		//get name value
+		//push it to data class that pulls from API and arranges into data format
+		//returns it here on route access
+		//return args -> {
+			//Quote quote = restTemplate.getForObject("https://gistcdn.githack.com/ayan-b/ff0441b5a8d6c489b58659ffb849aff4/raw/e1c5ca10f7bea57edd793c4189ea8339de534b45/response.json", Quote.class);
+			//log.info(quote.toString());
+			// https://openlibrary.org/developers/api
+			// https://openlibrary.org/search.json?title=vote+for+duck+click+clack+moo
+
+			RestTemplate restTemplate = new RestTemplate();
+			String fullSearchURL = String.format("https://openlibrary.org/search.json?q=%s", searchTerm);
+
+			Entry entry = restTemplate.getForObject(fullSearchURL, Entry.class);
+			log.info(entry.toString());
+			return String.format(entry.toString());
+		//};
 	}
 }
